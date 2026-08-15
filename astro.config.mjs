f485b7addd10c9dd706df1dd8a5b83029aa4bc96
Wojciech Smolarek <kontakt@wojciechsmolarek.pl>
2026-08-15 14:30:18 +0200

    add redirections + law pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wojciechsmolarek.pl',
+  site: 'https://www.wojciechsmolarek.pl',
   integrations: [
     react(),
     sitemap(),
